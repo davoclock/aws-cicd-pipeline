23e3b4eac1205e149ec5f803c80de01903a71696
David D <ddiaz@anthementertainment>
2020-12-29 16:53:22 -0500

    Updating branch from main to master

--- pipeline.tf
+++ pipeline.tf
@@ -70,7 +70,7 @@ resource "aws_codepipeline" "cicd_pipeline" {
             output_artifacts = ["tf-code"]
             configuration = {
                 FullRepositoryId = "davoclock/aws-cicd-pipeline"
-                BranchName   = "main"
+                BranchName   = "master"
                 ConnectionArn = var.codestar_connector_credentials
                 OutputArtifactFormat = "CODE_ZIP"
             }
