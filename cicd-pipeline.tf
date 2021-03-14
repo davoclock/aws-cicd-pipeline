661e55892674ab7c9c4a12377add3ce26e1b5429
David D <ddiaz@anthementertainment>
2021-03-14 12:08:57 -0400

    inception test

--- cicd-pipeline.tf
+++ cicd-pipeline.tf
@@ -1,5 +1,5 @@
 resource "aws_codebuild_project" "tf-plan" {
-  name          = "tf-cicd-plan"
+  name          = "tf-cicd-plan2"
   description   = "Plan stage for terraform"
   service_role  = aws_iam_role.tf-codebuild-role.arn
 
--- cicd-s3.tf
+++ cicd-s3.tf
@@ -1,4 +1,4 @@
 resource "aws_s3_bucket" "codepipeline_artifacts" {
   bucket = "pipeline-artifacts-davo"
   acl    = "private"
-}
+} 
