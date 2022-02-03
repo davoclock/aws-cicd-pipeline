7656395d5e52a0dad92e70349183e691e43896d9
pavelfilippov <p.g.filippov@gmail.com>
2022-02-03 22:14:36 +1300

    Demo vpc.

--- env.mk
+++ env.mk
@@ -4,6 +4,5 @@ COST_GROUP ?= demo-cicd
 TF_UPGRADE ?= false
 TF_ARGS =	-var="application_name=${APPLICATION_NAME}" \
 			-var="environment=${ENVIRONMENT}" \
-			-var='tags={"costgroup":"${COST_GROUP}"}' \
-#			-target='module.cicd'
+			-var='tags={"costgroup":"${COST_GROUP}"}'
 TF = AWS_SDK_LOAD_CONFIG=true terraform
--- modules/infrastructure/main.tf
+++ modules/infrastructure/main.tf
@@ -1,4 +1,6 @@
 resource "aws_vpc" "default" {
-  name: "demo-vpc"
   cidr_block = "1.2.0.0/16"
+  tags = {
+    Name = "demo-vpc-1"
+  }
 }
