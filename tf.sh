1a3bf0f73cde33dfbf0c630029b82f14f906b851
pavelfilippov <p.g.filippov@gmail.com>
2022-02-04 14:21:55 +1300

    Permission for the tf.sh.

--- tf.sh
+++ tf.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 # make sure that all mandatory parameters are set
-if [ -z "$AWS_PROFILE" ]
-then
-    echo "AWS_PROFILE is not defined. Please execute first: $ export AWS_PROFILE=your_profile_name"
-    exit 1
-fi
-
 if [ -z "$AWS_DEFAULT_REGION" ]
 then
     echo "AWS_DEFAULT_REGION is not defined. Please execute first: $ export AWS_DEFAULT_REGION='ap-southeast-2'"
