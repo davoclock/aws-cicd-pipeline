153a02ec485b2e672729256c4fb421a6d9bfb6fe
pavelfilippov <p.g.filippov@gmail.com>
2022-02-04 15:56:33 +1300

    Add extra tags to the VPC.

--- modules/infrastructure/main.tf
+++ modules/infrastructure/main.tf
@@ -1,9 +1,7 @@
 resource "aws_vpc" "default" {
-  cidr_block = "1.2.0.0/32"
+  cidr_block = "1.2.0.0/28"
 
   tags = merge({
-    application-name = var.application_name
-    env              = var.environment
     Name = "${var.environment}-${var.application_name}"
   }, var.tags)
 }
